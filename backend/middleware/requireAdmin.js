e1fdbc5002f7aab06dce7be0ba779a1da45203e9
samvaarv <samvaarv.vid@gmail.com>
2024-10-18 14:18:44 -0400

    change the user model import

--- backend/middleware/requireAdmin.js
+++ backend/middleware/requireAdmin.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
+import User from "../models/userModel.js";
 
 export const requireAdmin = (req, res, next) => {
   if (req.user && req.user.role === "admin") {
